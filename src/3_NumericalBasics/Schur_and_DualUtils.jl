d67d4080fec51fefcaa1916dddeea306117fb671
Ralph Luetticke <rcluetticke@gmail.com>
2023-05-24 16:01:44 +0200

    Update Hessian and Schur
    Switch to complex schur and finite differences for the Hessian.

--- src/3_NumericalBasics/Schur_and_DualUtils.jl
+++ src/3_NumericalBasics/Schur_and_DualUtils.jl
@@ -10,7 +10,15 @@ function real_schur(A, B)
     nk = sum(select_ev) # Number of state Variables based on Eigenvalues
     return F, select_ev, nk, λ
 end
-
+function complex_schur(A, B)
+    F = LinearAlgebra.schur(complex(A), complex(B))
+    α::Vector{complex(promote_type(eltype(A), eltype(B)))} = F.alpha
+    λ = abs.(α) ./ abs.(F.beta)
+    select_ev = λ .>= 1.0
+    # select_ev = abs.(λ) .>= 1.0
+    nk  = sum(select_ev) # Number of state Variables based on Eigenvalues
+    return F, select_ev, nk, λ
+end
 
 function tot_dual(x::ForwardDiff.Dual)
     a = sum(ForwardDiff.partials(x, :))
