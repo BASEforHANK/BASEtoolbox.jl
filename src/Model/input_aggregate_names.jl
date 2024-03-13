3b8a48047c655a29aa2598e38f76a716346b3373
Christian Bayer <christian.bayer@uni-bonn.de>
2024-03-13 15:00:46 +0100

    parser improvement
    Allow to automatically create replications of model equations for, say, many countries

--- src/Model/input_aggregate_names.jl
+++ src/Model/input_aggregate_names.jl
@@ -4,9 +4,13 @@
 # contained in the aggregate model code as they are parameter free but change whenever the 
 # distribution changes and do not show up in any aggregate model equation.
 
+n_rep = 0 # Number of n_rep of some model equations (e.g. countries, industries)
+
+# List of aggregate shocks, without duplication (e.g. across countries or industries)
 shock_names = [:Z, :ZI, :μ, :μw, :A, :Rshock, :Gshock, :Tprogshock, :Sshock]
 
-
+# List of aggregate states, without duplication of names (e.g. across countries or industries)
+# Duplicated names are created below
 state_names = [
     "A",
     "Z",
@@ -31,7 +35,13 @@ state_names = [
     "Sshock",
 ]
 
+# List of (the subset) of aggregate states, that need to be duplicated (e.g. across countries or industries)
+dup_state_names = ["",
+#     "A",
+]
+
 # List cross-sectional controls / distributional summary variables (no equations in aggregate model expected)
+# if these need to be duplicated, do by hand !
 distr_names = ["GiniC", "GiniW", "TOP10Ishare", "TOP10Inetshare", "TOP10Wshare", "sdlogy"]
 
 control_names = [
@@ -74,6 +84,31 @@ control_names = [
     "unionprofits",
     "profits",
 ]
+
+# List of (the subset) of aggregate states, that need to be duplicated (e.g. across countries or industries)
+dup_control_names = ["",
+#     "LP",
+]
+
+# Delete duplicated state and shock names and create duplicated variable names of state and control names 
+
+state_names = setdiff(state_names,dup_state_names)
+for  j = 1:n_rep # Create names of duplicated state variables (e.g. across countries or industries)
+    append!(state_names,dup_state_names.*string(j))
+end
+
+shock_states = string.(shock_names)
+dup_shock_states = intersect(string.(shock_names), dup_state_names)
+shock_states = setdiff(shock_states,dup_shock_states)
+for  j = 1:n_rep # Create names of duplicated shock variables (e.g. across countries or industries)
+    append!(shock_states ,dup_shock_states .*string(j))
+end
+shock_names = Symbol.(shock_states)
+
+control_names= setdiff(control_names,dup_control_names)
+for j = 1:n_rep # Create names of duplicated control variables (e.g. across countries or industries)
+    append!(control_names,dup_control_names.*string(j))
+end
 
 # All controls in one array
 control_names = [distr_names; control_names]
--- src/Preprocessor/PreprocessInputs.jl
+++ src/Preprocessor/PreprocessInputs.jl
@@ -6,16 +6,20 @@
 println("Preprocessing Inputs ...")
 
 # aggregate model
-f = open("Preprocessor/template_fcns/FSYS_agg.jl")
-lines = readlines(f)
-insert_index = findall(x -> x == "    # aggregate model marker", lines)[1]
+model_template_file = open("Preprocessor/template_fcns/FSYS_agg.jl")
+model_template_lines = readlines(model_template_file)
+insert_index = findall(x -> x == "    # aggregate model marker", model_template_lines)[1]
 
-g = open("Model/input_aggregate_model.jl")
-s = read(g, String)
+model_input_file = open("Model/input_aggregate_model.jl")
+model_input_text = read(model_input_file, String)
+model_input_file = open("Model/input_aggregate_model.jl")
+model_input_lines = readlines(model_input_file)
+
 deblank(S::String) = filter(x -> !isspace(x), S)
 number_of_equations =
-    length(collect(eachmatch(r"F\[indexes.", deblank(s)))) -
-    length(collect(eachmatch(r"\#F\[indexes.", deblank(s))))
+    length(collect(eachmatch(r"F\[indexes.", deblank(model_input_text)))) -
+    length(collect(eachmatch(r"\#F\[indexes.", deblank(model_input_text))))
+
 open("Preprocessor/generated_fcns/FSYS_agg_generated.jl", "w") do h
     println(
         h,
@@ -25,26 +29,42 @@ open("Preprocessor/generated_fcns/FSYS_agg_generated.jl", "w") do h
 
     println(h, "\n")
     for i = 1:insert_index-1
-        println(h, lines[i])
+        println(h, model_template_lines[i])
     end
     println(h, "\n")
-
-    write(h, s)
+    for i in model_input_lines  # input the model text
+        if occursin("@R", i)    # if there is a repetition marker create replications
+            n1      = findfirst("@R", i)
+            rsym    = i[n1[end]+1]
+            n2      = findfirst(" ", i[n1[end]+1:end])
+            repl    = parse(Int,i[n1[end]+2:n1[end]+n2[end]])
+            for j = 1:repl
+                line = i[n1[end]+n2[end]+1:end]
+                line = replace(line, rsym => string(j)) 
+                println(h, line)
+            end
+            if occursin("F\[indexes.", i)
+                number_of_equations += repl-1
+            end
+        else
+            println(h, i)
+        end
+    end
     println(h, "\n")
-    for i = insert_index+1:length(lines)
-        println(h, lines[i])
+    for i = insert_index+1:length(model_template_lines)
+        println(h, model_template_lines[i])
     end
 end
-close(f)
-close(g)
+close(model_template_file)
+close(model_input_file)
 
 # aggregate steady state
-f = open("Preprocessor/template_fcns/prepare_linearization.jl")
-lines = readlines(f)
-insert_index = findall(x -> x == "    # aggregate steady state marker", lines)[1]
+SS_template_file    = open("Preprocessor/template_fcns/prepare_linearization.jl")
+SS_template_lines   = readlines(SS_template_file)
+insert_index        = findall(x -> x == "    # aggregate steady state marker", SS_template_lines)[1]
 
-g = open("Model/input_aggregate_steady_state.jl")
-s = read(g, String)
+SS_input_file       = open("Model/input_aggregate_steady_state.jl")
+SS_input_text       = read(SS_input_file, String)
 
 open("Preprocessor/generated_fcns/prepare_linearization_generated.jl", "w") do h
     println(
@@ -54,18 +74,32 @@ open("Preprocessor/generated_fcns/prepare_linearization_generated.jl", "w") do h
     println(h, "\n")
 
     for i = 1:insert_index-1
-        println(h, lines[i])
+        println(h, SS_template_lines[i])
     end
     println(h, "\n")
     println(h, "@set! n_par.n_agg_eqn = $number_of_equations")
-    write(h, s)
+    for i in SS_input_lines  # input the model text
+        if occursin("@R", i)    # if there is a repetition marker create replications
+            n1      = findfirst("@R", i)
+            rsym    = i[n1[end]+1]
+            n2      = findfirst(" ", i[n1[end]+1:end])
+            repl    = parse(Int,i[n1[end]+2:n1[end]+n2[end]])
+            for j = 1:repl
+                line = i[n1[end]+n2[end]+1:end]
+                line = replace(line, rsym => string(j)) 
+                println(h, line)
+            end
+        else
+            println(h, i)
+        end
+    end
     println(h, "\n")
     for i = insert_index+1:length(lines)
-        println(h, lines[i])
+        println(h, SS_template_lines[i])
     end
 
 end
-close(f)
-close(g)
+close(SS_template_file)
+close(SS_input_file)
 
 println("Done!")
