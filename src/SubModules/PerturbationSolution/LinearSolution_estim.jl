a4315654f60fda93c936b87beb8a390b737c9267
Christian Bayer <christian.bayer@uni-bonn.de>
2024-04-26 09:38:03 +0200

    improve allocations in Linear_Solution_estim, remove type stability issues

--- src/SubModules/PerturbationSolution/LinearSolution_estim.jl
+++ src/SubModules/PerturbationSolution/LinearSolution_estim.jl
@@ -43,19 +43,29 @@ function LinearSolution_estim(
         zeros(2 * length_X0),
     )
 
-    for k in eachindex(aggr_names)
-        if !(any(distr_names .== aggr_names[k]))
-            j = getfield(sr.indexes, Symbol(aggr_names[k]))
-            for h in eachindex(aggr_names)
-                if !(any(distr_names .== aggr_names[h]))
-                    i = getfield(sr.indexes, Symbol(aggr_names[h]))
-                    A[j, i] = BA[k, h+length_X0]
-                    B[j, i] = BA[k, h]
-                end
-            end
-        end
-    end
-
+    # for k in eachindex(aggr_names)
+    #     if !(any(distr_names .== aggr_names[k]))
+    #         j = getfield(sr.indexes, Symbol(aggr_names[k]))
+    #         for h in eachindex(aggr_names)
+    #             if !(any(distr_names .== aggr_names[h]))
+    #                 i = getfield(sr.indexes, Symbol(aggr_names[h]))
+    #                 A[j, i] = BA[k, h+length_X0]
+    #                 B[j, i] = BA[k, h]
+    #             end
+    #         end
+    #     end
+    # end
+    mask::Vector{Bool}       = [!(aggr_names[k] in distr_names) for k in eachindex(aggr_names)]
+    indices::Vector{Int}     = [getfield(sr.indexes, Symbol(name)) for name in aggr_names[mask]]
+    index_XAggP::Vector{Int} = eachindex(aggr_names)[mask] .+ length_X0
+    index_XAgg::Vector{Int}  = eachindex(aggr_names)[mask]
+    # Select only the relevant parts of A,B, and
+    grid       = [CartesianIndex(i, j) for i in indices for j in indices]
+    grid_BA_A  = [CartesianIndex(i, j) for i in index_XAgg for j in index_XAggP]
+    grid_BA_B  = [CartesianIndex(i, j) for i in index_XAgg for j in index_XAgg]
+    A[grid]   .= BA[grid_BA_A] # copy only the non-distribution part of the Jacobian
+    B[grid]   .= BA[grid_BA_B] # copy only the non-distribution part of the Jacobian
+   
     ############################################################################
     # Solve the linearized model: Policy Functions and LOMs
     ############################################################################
